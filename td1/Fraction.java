52b2995336cf286038d8029d1b6e4fdbfb7508b1
Mfadi <72023685+uvsq21603504@users.noreply.github.com>
2020-09-28 16:27:41 +0200

    Update Fraction.java

--- td1/Fraction.java
+++ td1/Fraction.java
@@ -1,40 +1,35 @@
 public class Fraction
 
 {
-    public static void(String[] args)
-    {
+
         private int x;
         private int y;
         private double resultat;
 
-        public Fraction(int x,int y)
+        public Fraction( int x, int y)
         {
-            this.x=x;
-            this.y=y;
-            this.resultat=(double)x/y;
+            this.x = x;
+            this.y = y;
+            this.resultat = (double) x / y;
         }
 
-        public int getX()
-        {
-            return x;
-        }
+    public int getX() {
+        return x;
+    }
 
-        public int getY()
-        {
-            return y;
-        }
+    public int getY() {
+        return y;
+    }
 
-        public double getResultat()
-        {
-            return resultat;
-        }
+    public double getResultat() {
+        return resultat;
+    }
 
-        @Override
-        public String toString()
-        {
-            String s=x+" / "+y+" = "+resultat;
-            return s;
-        }
+    @Override
+    public String toString() {
+        String s = x + " / " + y + " = " + resultat;
+        return s;
+    }
 
 
 }
